672ed7e65ad5762bf825f34983373d8caa2ea86d
irakli22 <irakli1603@gmail.com>
2026-03-17 23:40:17 +0400

    0.4v module 12 done

--- src/app/components/recipe-list/recipe-list.ts
+++ src/app/components/recipe-list/recipe-list.ts
@@ -1,20 +1,34 @@
 import { Component, signal, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MOCK_RECIPES } from '../../mock-recipes';
-import { RecipeModel } from '../../models'; 
+import { RecipeModel } from '../../models';
 import { RecipeDetail } from '../recipe-detail/recipe-detail'
 
 @Component({
+  standalone: true,
   selector: 'app-recipe-list',
-  imports: [RecipeDetail],
+  imports: [RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
 export class RecipeList {
   protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
-  
-  protected setRecipe(index: number) {
-    this.recipe.set(MOCK_RECIPES[index]);
-    
-  }
+  protected readonly recipes = signal<RecipeModel[]>(MOCK_RECIPES);
+
+  protected search = signal("");
+
+  protected readonly filteredRecipes = computed(() => {
+    const term = this.search().toLowerCase();
+
+    if(term === ''){
+      return this.recipes();
+    }
+    return this.recipes().filter(recipe =>
+      recipe.name.toLowerCase().includes(term)
+    );
+  });
+  protected setRecipe(recipe: RecipeModel) {
+          this.recipe.set(recipe);
+        }
 
 }
